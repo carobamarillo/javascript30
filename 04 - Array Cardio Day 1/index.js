17fc8535362d7c7c3d6883fdea13cb7f66ce46d2
CaroAmarillo <caro_amarillo@hotmail.com>
2018-08-22 17:39:50 -0300

    Sort the inventors by years lived exercise

--- 04 - Array Cardio Day 1/index.js
+++ 04 - Array Cardio Day 1/index.js
@@ -70,6 +70,18 @@
 
     // 5. Sort the inventors by years lived
 
+    const oldest = inventors.sort((a, b) => {
+      
+      const lastGuy = a.passed - a.year;
+      const nextGuy = b.passed - b.year;
+
+     return lastGuy > nextGuy ? -1 : 1;
+         
+    } )
+
+    console.log('Sort the inventors by years lived ->');
+    console.table(oldest);
+
     // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
     // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
 
