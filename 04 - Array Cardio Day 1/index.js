b52efcd6fd45251824338b401be42ac660263d93
CaroAmarillo <caro_amarillo@hotmail.com>
2018-08-23 16:16:16 -0300

    Array Cardio exercise 8

--- 04 - Array Cardio Day 1/index.js
+++ 04 - Array Cardio Day 1/index.js
@@ -93,7 +93,7 @@
 
     // 7. sort Exercise
     // Sort the people alphabetically by last name
-      const alpha = people.sort(function (lastOne, nextOne) {
+      const alpha = people.sort((lastOne, nextOne) => {
         const [aLast, aFirst] = lastOne.split(', ');
         const [bLast, bFirst] = nextOne.split(', ');
         
@@ -105,3 +105,14 @@
     // 8. Reduce Exercise
     // Sum up the instances of each of these
     const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+
+    const transportation = data.reduce((obj, item) => {
+      if (!obj[item]) {
+        obj[item] = 0; 
+      }
+
+      obj[item]++;
+      return obj;
+    }, {})
+
+    console.log(transportation);
