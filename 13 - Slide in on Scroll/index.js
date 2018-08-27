add6a8191b4b5eebab10b89d394a13bec43b934b
CaroAmarillo <caro_amarillo@hotmail.com>
2018-08-27 10:51:41 -0300

    adding slide in on scroll finished files

--- 13 - Slide in on Scroll/index.js
+++ 13 - Slide in on Scroll/index.js
@@ -13,3 +13,25 @@ function debounce(func, wait = 20, immediate = true) {
 		if (callNow) func.apply(context, args);
 	};
 }
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+	sliderImages.forEach(sliderImage => {
+		// half way through the image 🚀
+		const slideInAt =
+			window.scrollY + window.innerHeight - sliderImage.height / 2;
+		// bottom of the image 🚀
+		const imageBottom = sliderImage.offsetTop + sliderImage.height;
+		const isHalfShown = slideInAt > sliderImage.offsetTop;
+		const isNotScrolledPast = window.scrollY < imageBottom;
+
+		if (isHalfShown && isNotScrolledPast) {
+			sliderImage.classList.add('active');
+		} else {
+			sliderImage.classList.remove('active');
+		}
+	});
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
