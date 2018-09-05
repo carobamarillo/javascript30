09f18026f37ca3a5713f339037932a45ae1082e2
CaroAmarillo <caro_amarillo@htomail.com>
2018-09-05 16:21:11 -0300

    sort bands names challenge finished files

--- 17 - Sort Without Articles/index.js
+++ 17 - Sort Without Articles/index.js
@@ -13,3 +13,10 @@ const bands = [
   'Anywhere But Here',
   'An Old Dog'
 ];
+
+function strip(bandName) {
+  return bandName.replace(/^(a |the |an)/i, '').trim();
+}
+const sorted = bands.sort((a, b) => (strip(a) > strip(b) ? 1 : -1));
+
+document.querySelector('#bands').innerHTML = sorted.map(band => `<li>${band}</li>`).join('');
