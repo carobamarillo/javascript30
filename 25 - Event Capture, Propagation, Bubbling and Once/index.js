ef070bf690e91e72e54321020063078738d1341c
Carolina Amarillo <caro_amarillo@hotmail.com>
2018-09-17 23:35:23 -0300

    event challenge finished files

--- 25 - Event Capture, Propagation, Bubbling and Once/index.js
+++ 25 - Event Capture, Propagation, Bubbling and Once/index.js
@@ -1,7 +1,13 @@
 const divs = document.querySelectorAll('div');
+const button = document.querySelector('button');
 
 function logtext(e) {
-	console.log(this.classList.value);
+		console.log(this.classList.value);
+		e.stopPropagation();
+
 }
 
-divs.forEach(div => div.addEventListener('click', logtext));
+divs.forEach(div => div.addEventListener('click', logtext, {capture: false}));
+button.addEventListener('click', () => {
+		console.log('Click!');
+}, {once: true})
